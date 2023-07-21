9cad99163dd82dffffad9956b835d85f64ca29e4
Mamdouh <Mamdohreda@gmail.com>
2023-07-21 16:03:43 +0100

    use google auto complete to retrun result

--- modules/search/controllers/SearchController.php
+++ modules/search/controllers/SearchController.php
@@ -10,7 +10,7 @@ use GuzzleHttp\Client;
 class SearchController extends Controller
 {
     protected array|bool|int $allowAnonymous = true;
-
+    
     public function actionSearch(): Response
     {
         $this->requirePostRequest();
@@ -20,43 +20,63 @@ class SearchController extends Controller
         $apiKey = getenv('API_KEY');
 
         $client = new Client();
-        $response = $client->request('GET', 'https://maps.googleapis.com/maps/api/place/textsearch/json', [
+        $response = $client->request('GET', 'https://maps.googleapis.com/maps/api/place/autocomplete/json', [
             'query' => [
-                'query' => $city,
+                'input' => $city,
+                'types' => '(cities)',
+                'components' => 'country:uk',
                 'key' => $apiKey,
             ],
         ]);
 
         $data = json_decode($response->getBody(), true);
 
-        // Check if results are present in the response
-        if (!isset($data['results']) || count($data['results']) == 0) {
+        if (!isset($data['predictions']) || count($data['predictions']) == 0) {
             return $this->asJson([
-                'error' => 'No results found for the given city. Please enter a valid UK city name.',
+                'error' => 'No results found for the given input. Please enter a valid UK city name.',
             ]);
         }
 
-        // Check if any result has "United Kingdom" or "UK" in the formatted_address
-        $ukResults = array_filter($data['results'], function ($result) {
-            return strpos($result['formatted_address'], 'United Kingdom') !== false ||
-                strpos($result['formatted_address'], 'UK') !== false;
-        });
-
-        // Check if there are any UK results
-        if (count($ukResults) === 0) {
-            return $this->asJson([
-                'error' => 'No results found for the given city in the UK. Please enter a valid UK city name.',
-            ]);
-        }
-
-        // Get the first UK result
-        $firstResult = reset($ukResults);
-        $location = $firstResult['geometry']['location'];
+        // Return the formatted addresses of the matched predictions and the place_id
+        $results = array_map(function ($prediction) {
+            return ['description' => $prediction['description'], 'place_id' => $prediction['place_id']];
+        }, $data['predictions']);
 
         return $this->asJson([
-            'city' => $firstResult['formatted_address'],
-            'longitude' => $location['lng'],
-            'latitude' => $location['lat'],
+            'predictions' => $results,
+        ]);
+    }
+
+    public function actionGetPlaceDetails(): Response
+    {
+        $this->requirePostRequest();
+        $placeId = Craft::$app->getRequest()->getBodyParam('place_id');
+
+        // Get the API key from the environment variable
+        $apiKey = getenv('API_KEY');
+
+        $client = new Client();
+        $response = $client->request('GET', 'https://maps.googleapis.com/maps/api/place/details/json', [
+            'query' => [
+                'place_id' => $placeId,
+                'fields' => 'name,geometry',
+                'key' => $apiKey,
+            ],
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+
+        if (isset($data['result']['geometry']['location'])) {
+            $location = $data['result']['geometry']['location'];
+            return $this->asJson([
+                'city' => $data['result']['name'],
+                'longitude' => $location['lng'],
+                'latitude' => $location['lat'],
+            ]);
+        }
+
+        return $this->asJson([
+            'error' => 'Failed to retrieve location details for the selected city.',
         ]);
     }
 }
